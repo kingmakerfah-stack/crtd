e52bd50d732123a5eaa6a307d6315e9942da8035
Gopal Patil <gopalpatil03709@gmail.com>
2026-04-04 17:46:08 +0530

    update API urls

--- applications/urls.py
+++ applications/urls.py
@@ -9,21 +9,21 @@ from .views import (ApplyJobView,CoolDownUpdateView,JobWiseSummaryView,
 
 urlpatterns = [
     path("apply-job/<int:id>/",ApplyJobView.as_view(),name="apply-job"),
-    path("student/my-applications/",StudentApplicationListView.as_view()),
+    path("student/my-applications/",StudentApplicationListView.as_view(),name="student-applications-list"),
 
 
     path("admin/cooling-period/update/",CoolDownUpdateView.as_view(),name="cooldown-days-update"),
 
-    path("admin/job-summary/",JobWiseSummaryView.as_view()),
+    path("admin/job-summary/",JobWiseSummaryView.as_view(),name="admin-job-summary"),
 
-    path("admin/job/<int:job_id>/", JobApplicationsView.as_view()),
+    path("admin/job/<int:job_id>/", JobApplicationsView.as_view(),name="admin-job-applications"),
 
-    path("admin/job/status/<int:id>",ApplicationDetailView.as_view()),
+    path("admin/job/status/<int:id>",ApplicationDetailView.as_view(),name="admin-application-detail"),
 
-    path("admin/update-status/<int:id>/",UpdateApplicationStatusView.as_view()),
+    path("admin/update-status/<int:id>/",UpdateApplicationStatusView.as_view(),name="admin-update-application-status"),
     
-    path("admin/dashboard/summary/",RealTimeActivitySummaryView.as_view()),
-    path("admin/update-cooldown/<int:id>/",UpdateStudentCoolDownView.as_view())
+    path("admin/realtime/summary/",RealTimeActivitySummaryView.as_view(),name="admin-dashboard-summary"),
+    path("admin/update-cooldown/<int:id>/",UpdateStudentCoolDownView.as_view(),name="admin-update-student-cooldown")
 
     
 ]
